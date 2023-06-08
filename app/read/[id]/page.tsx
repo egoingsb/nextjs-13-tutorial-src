777b99dcbd9206f5df212e0a3d3af67421ac234b
egoing <egoing@gmail.com>
2023-06-09 07:48:15 +0900

    읽기 기능 구현

--- app/read/[id]/page.tsx
+++ app/read/[id]/page.tsx
@@ -1,8 +1,12 @@
-export default function Read({ params }: { params: { id: string } }) {
+export default async function Read({ params }: { params: { id: string } }) {
+  const resp = await fetch(
+    process.env.NEXT_PUBLIC_API_URL + "topics/" + params.id
+  );
+  const data = await resp.json();
   return (
     <>
-      <h2>Read</h2>
-      params {params.id}
+      <h2>{data.title}</h2>
+      {data.body}
     </>
   );
 }
